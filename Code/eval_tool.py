cdf6b83d8dda807c2abfe40788e035b54b7c2562
BallBallQ <qjy980811@gmail.com>
2021-04-27 18:12:49 +0800

    commit code

--- Code/eval_tool.py
+++ Code/eval_tool.py
@@ -18,3 +18,34 @@ class Dice(nn.Module):
         dice = torch.mean(numerator / (denominator + self.eps))
 
         return dice
+
+
+def _fast_hist(label, pred, n_class):
+    mask = (label >= 0) & (label < n_class)
+    hist = np.bincount(
+        n_class * label[mask].astype(int) +
+        pred[mask], minlength=n_class ** 2).reshape(n_class, n_class)
+    return hist
+
+
+def eval_score(labels, preds, n_class):
+    hist = np.zeros((n_class, n_class))
+    for lt, lp in zip(labels, preds):
+        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    
+    # pixel accuracy
+    PA = np.diag(hist).sum() / hist.sum()
+
+    # mean pixel accuracy
+    MPA = np.diag(hist) / hist.sum(axis=1)
+    MPA = np.nanmean(MPA)
+
+    # mean intersection over union
+    IoU = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
+    MIoU = np.nanmean(IoU)
+
+    # frequency weighted intersection over union
+    freq = hist.sum(axis=1) / hist.sum()
+    FWIoU = (freq[freq > 0] * IoU[freq > 0]).sum()
+    return PA, MPA, MIoU, FWIoU
+    
--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,11 @@
 import argparse
 import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ["CUDA_VISIBLE_DEVICES"]='7'
+
 from train import train
 from test import test
+from utils import setup_seed
 
 parser = argparse.ArgumentParser()
 
@@ -10,8 +14,8 @@ parser.add_argument('--batch_size_train', type=int, default=16, help='batch size
 parser.add_argument('--batch_size_val', type=int, default=16, help='batch size for val')
 parser.add_argument('--batch_size_test', type=int, default=15, help='batch size for test')
 
-parser.add_argument('--input_width', type=int, default=256, help="x dimension of the image")
-parser.add_argument('--input_height', type=int, default=256, help="y dimension of the image")
+parser.add_argument('--input_width', type=int, default=300, help="x dimension of the image")
+parser.add_argument('--input_height', type=int, default=300, help="y dimension of the image")
 
 parser.add_argument('--num_classes', type=int, default=21, help='the number of the classes')
 
@@ -23,6 +27,8 @@ parser.add_argument('--beta_2', type=float, default=0.999, help="default beta_2 
 parser.add_argument('--model', type=str, default='model.pth', help="model save")
 
 parser.add_argument('--log', type=str, default='log.txt', help="text file to save training logs")
+parser.add_argument('--train', type=str, default='train.txt', help="text file to save train logs")
+parser.add_argument('--eval', type=str, default='eval.txt', help="text file to save evaluation logs")
 
 parser.add_argument('--start_epoch', type=int, default=0, help="flag to set the starting epoch for training")
 parser.add_argument('--end_epoch', type=int, default=500, help="flag to indicate the final epoch of training")
@@ -31,9 +37,7 @@ parser.add_argument('--unet_model', type=str, default="UNet", help="model")
 
 FLAGS = parser.parse_args()
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-# os.environ["CUDA_VISIBLE_DEVICES"]='7'
-
 if __name__ == '__main__':
+    setup_seed(99)
     train(FLAGS)
-    test(FLAGS)
+    # test(FLAGS)
